db52164ab8613a02973bfd022bf0d969cb65a02c
Camila Vigneaux <camila.vigneaux@thoughtworks.com>
2022-10-04 12:38:24 -0300

    [AUV-001][Cami]: Add api call to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductList from './components/ProductList';
 
-function App() {
+import { PRODUCTS } from './utils/constants'
+
+export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const config = {
+      origin: 'http://localhost:3000',
+      headers: {
+        "access-control-allow-origin" : "*",
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+      }}
+  useEffect(() => {
+    fetch(PRODUCTS, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData.items);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API Posts</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+        {data &&
+           <ProductList data={data}/>   
+          }
+      </ul>
     </div>
   );
 }
-
-export default App;
